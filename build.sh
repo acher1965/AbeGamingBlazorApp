947749ca403768aa146e7df8b50252a8d448281e
Alberto Cherubini <abecherubini@gmail.com>
2026-02-16 22:04:28 +0000

    change build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-curl -sSL  > dotnet-install.sh
+set -e 
+
+# --- CONFIGURATION ---
+# 1. Change this to your subfolder name (e.g., "ClientApp" or "MyBlazorApp")
+PROJECT_FOLDER="AbeGamingBlazorApp" 
+# ---------------------
+
+echo "Installing .NET 10.0..."
+
+# 1. Get the install script
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
-./dotnet/dotnet publish -c Release -o output
+
+# 2. Install .NET 10
+./dotnet-install.sh --channel 10.0 --install-dir ./dotnet-sdk
+
+# 3. Publish the project
+# This will put everything into a folder named 'dist' in the root
+echo "Building $PROJECT_FOLDER..."
+./dotnet-sdk/dotnet publish "$PROJECT_FOLDER" -c Release -o ./dist
