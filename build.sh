11e7afddf592ee0d655441b0a914afc625a9462a
Alberto Cherubini <abecherubini@gmail.com>
2026-02-16 22:38:09 +0000

    add changelist page

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --channel 10.0 --install-dir ./dotnet-sdk
 
+# --- GENERATE CHANGELOG FROM GIT COMMITS ---
+echo "Generating changelog from git commits..."
+git log --pretty=format:'{%n  "hash": "%H",%n  "shortHash": "%h",%n  "date": "%ci",%n  "author": "%an",%n  "message": "%s"%n},' HEAD | sed '$ s/,$//' | sed '1s/^/[/' | sed '$s/$/]/' > "$PROJECT_FOLDER/wwwroot/changelog.json"
+echo "Changelog generated with $(git rev-list --count HEAD) commits."
+
 echo "Building $PROJECT_FOLDER..."
 ./dotnet-sdk/dotnet publish "$PROJECT_FOLDER" -c Release -o ./dist
 
