8615cce93af39b19653a8f7160d15e0a69fe03fc
Alberto Cherubini <abecherubini@gmail.com>
2026-02-19 19:07:12 +0000

    WIP: improve exact stats; refactor

---
+++ AbeGaming.GameLogic/FtP/FtpStats.cs
@@ -0,0 +1,11 @@
+namespace AbeGaming.GameLogic.FtP
+{
+    public record FtpStats(
+    BattleSize BattleSize,
+    double AttackerWinProbability,
+    double DefenderWinProbability,
+    HitStats HitsStats,
+    double AttackerLeaderDeathProbability,
+    double DefenderLeaderDeathProbability,
+    double StarResultProbability);
+}
